e6126be02282aeedf603d6f482c6bf07e4455906
gritNgo <dustspeck00@gmail.com>
2024-05-09 07:47:40 +0200

    Better late than never: Commit of finished project

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Stack stack = new Stack();
+
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+stack.Push(null);
+
+foreach (var item in stack.Objects)
+{
+    Console.WriteLine(item + " pushed");
+}
+
+Console.WriteLine();
+Console.WriteLine(stack.Pop() + " popped");
+Console.WriteLine(stack.Pop() + " popped");
+Console.WriteLine(stack.Pop() + " popped");
+Console.WriteLine(stack.Pop());
+
+
+stack.Clear();
+
---
+++ Stack/Stack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+public class Stack
+{
+    public object obj { get; set; }  
+    public readonly List<object> Objects;
+
+    public Stack()
+    {
+        Objects = new List<object>();
+    }
+
+
+    public void Push(object obj)
+    {
+        try
+        {
+            if (obj != null)
+            {
+                Objects.Add(obj);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        catch (InvalidOperationException)
+        {
+            Console.WriteLine("Null cannot be added into the stack");
+        }
+    }
+
+    public object Pop()
+    {
+        try
+        {
+            if (Objects.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        catch (InvalidOperationException)
+        {
+            Console.WriteLine("Pop() cannot be called on an empty stack");
+            return obj;
+        }
+
+        var popped = Objects[Objects.Count - 1];
+        Objects.RemoveAt(Objects.Count - 1);
+        return popped;
+    }
+
+    public void Clear()
+    {
+        Objects.Clear();
+    }
+}
